aebba4afa566300ed8e2b49b02bcd0f7d419b1ce
gnartz <157133953+gnartz@users.noreply.github.com>
2024-02-23 15:52:16 -0500

    bump version

--- gnartz_webproxy/setup.py
+++ gnartz_webproxy/setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='gnartz_webproxy',
-    version='0.1.0',
+    version='0.2.0',
     author='Jimmy Allen',
     author_email='allenjsomb@gmail.com',
     description='A simple async web proxy.',
